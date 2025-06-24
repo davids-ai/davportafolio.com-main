eb7dbcc184eeb7dc8958991fcabf9ea4f558bac5
David <dsrojaslop@gmail.com>
2025-06-24 14:29:24 -0500

    Install githubpages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
-import createMDX from '@next/mdx'
-import rehypeShiki from '@shikijs/rehype'
-import createNextIntlPlugin from 'next-intl/plugin';
+import createMDX from '@next/mdx';
+import rehypeShiki from '@shikijs/rehype';
+import createNextIntlPlugin from 'next-intl/plugin.js';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // 👈 Necesario para GitHub Pages
+  basePath: '/davportafolio.com-main', // 👈 Coincide con el nombre del repo
+  trailingSlash: true, // 👈 Asegura compatibilidad con rutas estáticas
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-}
+};
 
 const withMDX = createMDX({
   options: {
@@ -17,10 +20,8 @@ const withMDX = createMDX({
       }
     }]],
   },
-})
+});
 
-const withNextIntl = createNextIntlPlugin(
-  './src/i18n.ts'
-);
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
-export default withMDX(withNextIntl(nextConfig))
+export default withMDX(withNextIntl(nextConfig));
